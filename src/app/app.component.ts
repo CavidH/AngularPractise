9d67147e19f3ea2dfa4a18113ac03b8aab90ba7b
Cavid Haciyev <javid.hajiyev@ziraatbank.az>
2023-02-08 10:58:02 +0400

    :cactus:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
- // templateUrl: './app.component.html',
-  template:`<br><strong>Hellor</strong>`,
-  styleUrls: ['./app.component.css']
+  // templateUrl: './app.component.html',
+  template: `<br><strong>Hello<br>{{title}}</strong>
+  <hr> <br><strong>{{age|currency}}</strong> `,
+  // styleUrls: ['./app.component.css']
+  styles: [`strong{color: cyan}`]
 })
 export class AppComponent {
   title = 'AngularPractise';
-  age:number=45;
+  age: number = 45;
 }
