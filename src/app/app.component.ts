7684187ca5e06aec3efc82f500c9433aa036a174
Cavid Haciyev <javid.hajiyev@ziraatbank.az>
2023-02-08 14:22:05 +0400

    :cactus: Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,14 @@ import {Component} from '@angular/core';
   selector: 'app-root1',
   // templateUrl: './app.component.html',
   template:  `
-  <p>{{title}}</p>
-  <img [src]="src"  [width]="width"  >
-<!--   <img src="{{src}}" [width]="width"  >-->
+      <p>{{title}}</p>
+
+      <input type="text" [value]="inputContent">
+
+      <button>Click</button><br>
+
+      <img [src]="src" [width]="width">
+      <!--   <img src="{{src}}" [width]="width"  >-->
   ` ,
   // styleUrls: ['./app.component.css']
   styles: [`strong{color: cyan}`]
@@ -15,4 +20,5 @@ export class AppComponent {
   title = 'Angular';
   src:string='https://angular.io/assets/images/logos/angular/angular.png';
   width:number=150;
+  inputContent:string="Salam";
 }
