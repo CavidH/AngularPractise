d1507badbe8c27b6340005de5e8ffc81a0f1a611
Cavid Haciyev <javid.hajiyev@ziraatbank.az>
2023-02-08 14:41:03 +0400

    :cactus: Property Binding simple counter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,30 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-root1',
   // templateUrl: './app.component.html',
-  template:  `
+  template: `
       <p>{{title}}</p>
 
       <input type="text" [value]="inputContent">
+      <br><br>
+      <button (click)="change()">Click Count {{count}}</button><br>
 
-      <button>Click</button><br>
 
       <img [src]="src" [width]="width">
       <!--   <img src="{{src}}" [width]="width"  >-->
-  ` ,
+  `,
   // styleUrls: ['./app.component.css']
-  styles: [`strong{color: cyan}`]
+  styles: [`strong {
+    color: cyan
+  }`]
 })
 export class AppComponent {
   title = 'Angular';
-  src:string='https://angular.io/assets/images/logos/angular/angular.png';
-  width:number=150;
-  inputContent:string="Salam";
+  count:number=0;
+  src: string = 'https://angular.io/assets/images/logos/angular/angular.png';
+  width: number = 150;
+  inputContent: string = "Salam";
+
+  change() {
+    this.count++;
+  }
 }
