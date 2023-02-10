8e0805e52518a72fbd2e312fc41e5c42140c585e
Cavid Haciyev <javid.hajiyev@ziraatbank.az>
2023-02-10 16:51:14 +0400

    :cactus:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,24 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root1',
-  // templateUrl: './app.component.html',
-  template: `<h1>sdlfkdspkfl</h1>  `,
-  // styleUrls: ['./app.component.css']
-  styles: []
+  template: `<h1>sdlfkdspkfl</h1>
+
+  <p [ngClass]="{segoePrintFont:isSegoe}">
+    Angular modul proqram arxitekturasını təqdim edir  </p>
+  `,
+  styles: [
+    `.verdanaFont {
+      font-size: 13px;
+      font-family: Verdana;
+    }
+
+    .segoePrintFont {
+      font-size: 14px;
+      font-family: "Segoe Print";
+    }`
+  ]
 })
 export class AppComponent {
-
+  isVerdana = true;
+  isSegoe = true;
 }
