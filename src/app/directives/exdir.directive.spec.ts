fe08c11e6270f7e835d015159c6ae98cef6a741f
Cavid Haciyev <javid.hajiyev@ziraatbank.az>
2023-02-10 16:36:11 +0400

    :cactus:

---
+++ src/app/directives/exdir.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ExdirDirective } from './exdir.directive';
+
+describe('ExdirDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ExdirDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/directives/exdir.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appExdir]'
+})
+export class ExdirDirective {
+
+  constructor() { }
+
+}
